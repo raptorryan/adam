e53a919aa60742b854bd545e3d28bc2d98295c13
Jonathan Caprell <jbcaprell@diacritical.net>
2023-03-11 19:30:00 -0500

    Create `v0.6.4` release

--- mix.exs
+++ mix.exs
@@ -74,7 +74,7 @@ defmodule Adam.MixProject do
       :adam
 
       iex> project()[:version]
-      "0.6.3"
+      "0.6.4"
 
   """
   @doc since: "0.1.0"
@@ -120,7 +120,7 @@ defmodule Adam.MixProject do
       name: "Adam",
       source_url: "https://github.com/diacritical/adam",
       start_permanent: Mix.env() == :prod,
-      version: "0.6.3"
+      version: "0.6.4"
     ]
   end
 
